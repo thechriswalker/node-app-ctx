c2812b9457efc672bbea9654310f63151b0129bf
Chris Walker <chris@thechriswalker.net>
2017-07-21 16:47:44 +0100

    Fix bad reference

--- express.js
+++ express.js
@@ -2,51 +2,53 @@
 exports.middleware = (
     baseContext,
     { send = defaultSend, logTiming = defaultLogTiming, exposeId = true } = {}
-) => (req, res, next) => {
-    req.withContext = async fn => {
-        let hijacked = false;
-        const hijack = () => (hijacked = true);
-        const childContext = baseContext.child();
-        let result;
-        try {
-            result = await fn(childContext, hijack, req, res, next);
-        } catch (err) {
-            result = err;
-        }
-
-        if (!hijacked) {
-            if (logTiming) {
-                const timing = childContext.lifetime().toFixed(3);
-                res.set("x-request-time", timing + "ms");
-                if (typeof logTiming === "function") {
-                    logTiming(ctx, timing, req, res);
+) => {
+    const idHeader = typeof exposeId === "string" ? exposeId : "x-request-id";
+    return (req, res, next) => {
+        req.withContext = async fn => {
+            let hijacked = false;
+            const hijack = () => (hijacked = true);
+            const childContext = baseContext.child();
+            let result;
+            try {
+                result = await fn(childContext, hijack, req, res, next);
+            } catch (err) {
+                result = err;
+            }
+            if (!hijacked) {
+                if (logTiming) {
+                    const timing = childContext.lifetime().toFixed(3);
+                    res.set("x-request-time", timing + "ms");
+                    if (typeof logTiming === "function") {
+                        logTiming(childContext, timing, req, res);
+                    }
+                }
+                if (exposeId) {
+                    res.set(idHeader, childContext.id);
+                }
+                send(req, res, result);
+                await childContext.done();
+            } else if (!childContext.isDone) {
+                // whoa there cowboy! you MUST release the context yourself if hijacking!
+                const id = childContext.id;
+                // the context was not "done" by the time the hijacked thing finished.
+                // we wait 10 seconds and then warn.
+                await new Promise(r => setTimeout(r, 10e3));
+                if (!childContext.isDone) {
+                    //ok. lets warn.
+                    console.warn(
+                        "`ctx.done()` not called after hijacking the middleware. Please check your route config",
+                        {
+                            method: req.method.toUpperCase(),
+                            uri: req.originalUrl,
+                            ctxId: id
+                        }
+                    );
                 }
             }
-            if (exposeId) {
-                res.set("x-request-id", childContext.id);
-            }
-            send(req, res, result);
-            await childContext.done();
-        } else if (!childContext.isDone) {
-            // whoa there cowboy! you MUST release the context yourself if hijacking!
-            const id = childContext.id;
-            // the context was not "done" by the time the hijacked thing finished.
-            // we wait 10 seconds and then warn.
-            await new Promise(r => setTimeout(r, 10e3));
-            if (!childContext.isDone) {
-                //ok. lets warn.
-                console.warn(
-                    "`ctx.done()` not called after hijacking the middleware. Please check your route config",
-                    {
-                        method: req.method.toUpperCase(),
-                        uri: req.originalUrl,
-                        ctxId: id
-                    }
-                );
-            }
-        }
+        };
+        next();
     };
-    next();
 };
 
 // this does very little, but often it is enough.
